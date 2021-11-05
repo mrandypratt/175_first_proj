871f61d15bd39d4686b693cb61106a52ed163c07
MrAndyPratt <mrandypratt@gmail.com>
2021-11-04 23:15:41 -0500

    Refactored Express logic using paramater value and input validation.  Also added error handler.

--- hello.js
+++ hello.js
@@ -24,6 +24,22 @@ const COUNTRY_DATA = [
   },
 ];
 
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+};
+
+const helloWorld = (view, language) => {
+  return (req, res) => {
+    res.render(view, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: language,
+    });
+  };
+};
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
@@ -34,32 +50,23 @@ app.locals.currentPathClass = (path, currentPath) => {
   return path === currentPath ? "current" : "";
 };
 
-app.get("/", (req, res) => {
-  res.redirect("/english");
+app.get("/:language", (req, res) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`));
+  } else {
+    res.render(`hello-world-${language}`, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: languageCode,
+    });
+  }
 });
 
-app.get("/english", (req, res) => {
-  res.render("hello-world-english", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "en-US",
-  });
-});
-
-app.get("/french", (req, res) => {
-  res.render("hello-world-french", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "fr-FR",
-  });
-});
-
-app.get("/serbian", (req, res) => {
-  res.render("hello-world-serbian", {
-    countries: COUNTRY_DATA,
-    currentPath: req.path,
-    language: "sr-Cyrl-rs",
-  });
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
 });
 
 app.listen(PORT, "localhost", () => {
